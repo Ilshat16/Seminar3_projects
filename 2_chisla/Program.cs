6db6083bce5d31e3e92de52a6addd2f00774f341
Ilshat <illliat@mail.ru>
2022-11-29 21:14:41 +0300

    Tasks1, 2

---
+++ 2_chisla/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите первое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int result = number1 * number1;
+if (result == number2)
+{
+    Console.WriteLine($"{number1} является квадратом числа {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1} не является квадратом числа {number2}");
+}
---
+++ dni_nedeli/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите номер дня: ");
+string day = Console.ReadLine();
+if (day == "1") Console.Write("Понедельник");
+if (day == "2") Console.Write("Вторник");
+if (day == "3") Console.Write("Среда");
+if (day == "4") Console.Write("Четверг");
+if (day == "5") Console.Write("Пятница");
+if (day == "6") Console.Write("Суббота");
+if (day == "7") Console.Write("Воскресенье");
+else Console.Write("Такого дня недели нет");
