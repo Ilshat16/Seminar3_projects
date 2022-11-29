57f88fc05a5c07abb2847d049dad404bd8312083
Ilshat <illliat@mail.ru>
2022-11-29 21:58:53 +0300

    add tasks 3, 4

---
+++ Task2/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+int result = number % 10;
+Console.WriteLine($"Последней цифрой числа {number} является - {result}");
---
+++ task1/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int num1 = -number;
+while (num1 <= number)
+{
+    Console.Write($"{num1}, ");
+    num1++;
+}
