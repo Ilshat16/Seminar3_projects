2ca8c06a137a19426d699d5ee053922754e80e8d
Ilshat <illliat@mail.ru>
2022-12-02 20:31:05 +0300

    Random Number

---
+++ RandomNum/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int number = new Random().Next(10, 100);
+int n1 = number / 10;
+int n2 = number % 10;
+int max = n1;
+if (n2 > n1) max = n2;
+Console.WriteLine($"В числе {number} максимальным является {max}");
