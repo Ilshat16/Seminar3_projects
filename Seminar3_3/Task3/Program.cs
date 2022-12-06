bea51fe32963a2ea0628fd1d565143dd84919266
Ilshat <illliat@mail.ru>
2022-12-06 21:38:29 +0300

    add Task3 S3_3

---
+++ Seminar3_3/Task3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Методы
+double FindDistance(int x1, int y1, int x2, int y2)
+{
+    double result = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    return result;
+}
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+//Запрос координат
+int x1 = EnterData("Введите координаты Х1: ");
+int y1 = EnterData("Введите координаты Y1: ");
+int x2 = EnterData("Введите координаты Х2: ");
+int y2 = EnterData("Введите координаты Y2: ");
+//Анализ данных
+double result = FindDistance(x1, y1, x2, y2);
+//Вывод информации
+Console.WriteLine($"Расстояние между данными точками равно {result}");
